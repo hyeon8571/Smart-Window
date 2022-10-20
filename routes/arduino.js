cdfc58c8e793f099f5f477254fc8f04a6a35f819
hyeon8571 <w01085914442@gmail.com>
2022-10-20 17:43:15 +0900

    server commit

--- routes/arduino.js
+++ routes/arduino.js
@@ -25,15 +25,19 @@ router.get('/register', (req, res) => {
     })
 })
 
-router.get('/sensing', (req, res) => {
+router.post('/sensing', (req, res) => {
     req.app.db.collection('data-serial').findOne({serialNum : req.query.serialNum}, (에러, 결과) => {
-        if (결과 != null) {
-            req.app.db.collection('data-serial').updateOne({serialNum : req.query.serialNum}, {$set : {temperature : req.query.temp, humidity : req.query.humid, rain : req.query.rain, gas : req.query.gas, state : req.query.state}}, (에러, 결과) => {
+        var updated = {temperature : req.query.temp, humidity : req.query.humid, rain : req.query.rain, gas : req.query.gas, state : req.query.state};
 
+        var inserted = {serialNum : req.query.serialNum, temperature : req.query.temp, humidity : req.query.humidity, rain : req.query.rain, gas : req.query.gas, state : req.query.state, dust : "20"};
+
+        if (결과 != null) {
+            req.app.db.collection('data-serial').updateOne({serialNum : req.query.serialNum}, {$set : updated}, (에러, 결과) => {
+                res.json("update!!");
             })
         } else {
-            req.app.db.collection('data-serial').insertOne({serialNum : req.query.serialNum, temperature : req.query.temp, humidity : req.query.humidity, rain : req.query.rain, gas : req.query.gas, state : req.query.state, dust : "20"}, (에러, 결과) => {
-
+            req.app.db.collection('data-serial').insertOne(inserted, (에러, 결과) => {
+                res.json("insert!!");
             })
         }
     })
