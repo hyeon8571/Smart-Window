af4d1290e02d3563780675315d03f901beeebbaa
hyeon8571 <w01085914442@gmail.com>
2022-10-20 18:06:41 +0900

    server commit

--- routes/arduino.js
+++ routes/arduino.js
@@ -29,15 +29,17 @@ router.get('/sensing', (req, res) => {
     req.app.db.collection('data-serial').findOne({serialNum : req.query.serialNum}, (에러, 결과) => {
         var updated = {temperature : req.query.temp, humidity : req.query.humid, rain : req.query.rain, gas : req.query.gas, state : req.query.state};
 
-        var inserted = {serialNum : req.query.serialNum, temperature : req.query.temp, humidity : req.query.humidity, rain : req.query.rain, gas : req.query.gas, state : req.query.state, dust : "20"};
+        var inserted = {serialNum : req.query.serialNum, temperature : req.query.temp, humidity : req.query.humid}
+
+        //, rain : req.query.rain, gas : req.query.gas, state : req.query.state, dust : "20"};
 
         if (결과 != null) {
             req.app.db.collection('data-serial').updateOne({serialNum : req.query.serialNum}, {$set : updated}, (에러, 결과) => {
-                
+                res.json("update!!")
             })
         } else {
             req.app.db.collection('data-serial').insertOne(inserted, (에러, 결과) => {
-                
+                res.json("insert!!")
             })
         }
     })
