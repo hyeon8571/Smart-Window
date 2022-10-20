013bb7d729b018a400964221c03c4d549452a9ec
hyeon8571 <w01085914442@gmail.com>
2022-10-20 17:07:32 +0900

    server commit

--- routes/arduino.js
+++ routes/arduino.js
@@ -12,10 +12,21 @@ router.get('/register', (req, res) => {
                     res.json({check : "true"});
                 } 
         })
-    }else {
-        res.json({check : "true"});
-    }
+        }else {
+            res.json({check : "true"});
+        }
 })
+    req.app.db.collection('mode').insertOne({serialNum : req.query.serialNum, automode : 'on'}, (에러, 결과) => {
+
+    })
+
+    req.app.db.collection('option').insertOne({serialNum : req.query.serialNum, temp : '27', humid : '70', dust : '20'}, (에러, 결과) => {
+        
+    })
+})
+
+router.get('/sensing', (req, res) => {
+    req.app.db.collection('data-serial').updateOne({serialNum : req.query.serialNum}, {$set : {temperature : req.query.temp, humidity : req.query.humid, rain : req.query.rain, gas : req.query.gas, state : req.query.state}})
 })
 
 
